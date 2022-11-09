1a64f7b8e56ed667a3dd5623e6776dbf00ac2d74
Gabriel Berti <gabriel.berti@linainfratech.com.br>
2022-11-09 20:40:54 -0300

    03. Conectando com o MongoDB - Nessa aula, aprendemos: A diferença entre bancos de dados relacionais e NoSQL; Como utilizar o MongoDB Atlas, que é o serviço de banco de dados na nuvem, onde você pode escolher hospedar seu banco em plataformas como Google Cloud Platform, AWS ou Azure; Instalar e utilizar a biblioteca Mongoose que é uma solução baseada em esquemas (Schemas) para modelar o banco de dados; Criar o schema de livros, para representar a coleção livros no banco de dados, definindo o como será o documento armazenado e os campos requeridos; Fazer a conexão da API com o banco de dados, verificando se a mesma ocorreu com sucesso ou não.

--- src/app.js
+++ src/app.js
@@ -1,23 +1,24 @@
 import express from "express";
+import db from "./config/dbConnect.js";
+import livros from './models/Livro.js'
+
+db.on("error", console.log.bind(console, "Erro de Conexão"));
+db.once("open", () => {
+  console.log("Conexão com o banco feita com sucesso.");
+});
 
 const app = express();
 
 app.use(express.json());
-
-const livros = [
-  {
-    id: 1,
-    titulo: "Senhor dos Anéis",
-  },
-  { id: 2, titulo: "O Hobbit" },
-];
 
 app.get("/", (req, res) => {
   res.status(200).send("Curso de Node");
 });
 
 app.get("/livros", (req, res) => {
-  res.status(200).json(livros);
+  livros.find((err, livros) => {
+    res.status(200).json(livros);
+  })
 });
 
 app.get("/livros/:id", (req, res) => {
@@ -36,12 +37,12 @@ app.put("/livros/:id", (req, res) => {
   res.json(livros);
 });
 
-app.delete('/livros/:id', (req,res) => {
-  let {id} = req.params;
+app.delete("/livros/:id", (req, res) => {
+  let { id } = req.params;
   let index = buscaLivro(id);
   livros.splice(index, 1);
-  res.send(`Livro ${id} deletado com sucesso.`)
-})
+  res.send(`Livro ${id} deletado com sucesso.`);
+});
 
 function buscaLivro(id) {
   return livros.findIndex((livro) => livro.id == id);
